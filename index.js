d475d738bc738fe3872d61528d39ed0693fb00ec
thoque579 <tusharhoque2@gmail.com>
2021-04-06 16:33:50 -0700

    connected to ajax server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('js is working');
+$.ajax({
+  type: 'GET',
+  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+  dataType: 'json',
+  success: function (response, textStatus) {
+    console.log(response);
+  },
+  error: function (request, textStatus, errorMessage) {
+    console.log(errorMessage);
+  }
+});
