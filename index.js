e4f0974a7a11ab0e4d13598d450c560d2785ecdb
thoque579 <tusharhoque2@gmail.com>
2021-04-06 16:51:32 -0700

    added document ready function and tested if iteration between tasks

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-$.ajax({
-  type: 'GET',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-  dataType: 'json',
-  success: function (response, textStatus) {
-    console.log(response);
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function (task) {
+        $('#todo-list').append('<p>' + task.content + '</p>');
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 });
