ae959562ca209f4cd9e5a6161ec673448fbb48e2
thoque579 <tusharhoque2@gmail.com>
2021-04-06 16:55:48 -0700

    created new id for adtpi and added ajax request for creating new tasks

--- index.js
+++ index.js
@@ -12,4 +12,24 @@ $(document).ready(function(){
       console.log(errorMessage);
     }
   });
+
+
+  $.ajax({
+  type: 'POST',
+  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=47',
+  contentType: 'application/json',
+  dataType: 'json',
+  data: JSON.stringify({
+    task: {
+      content: 'Do something fun'
+    }
+  }),
+  success: function (response, textStatus) {
+    console.log(response);
+  },
+  error: function (request, textStatus, errorMessage) {
+    console.log(errorMessage);
+  }
+});
+
 });
