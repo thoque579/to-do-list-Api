9c055fb73907bfc3769f949c7a7cbea19edf0fd8
thoque579 <tusharhoque2@gmail.com>
2021-04-06 20:45:28 -0700

    added create task, delete task, task completetion, connect to html

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-$(document).ready(function()
-  {
-    $.ajax(
-    {
+$(document).ready(function(){
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
       type: 'GET',
-      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=50',
       dataType: 'json',
       success: function (response, textStatus) {
-      response.tasks.forEach(function (task) {
-      $('#todo-list').append('<p>' + task.content + '</p>');
-      })
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  });
-
-
-  $.ajax(
-  {
-      type: 'POST',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=47',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-    task:
-    {
-      content: 'Do something fun'
-    }
-  }),
-    success: function (response, textStatus)
-  {
-    console.log(response);
-  },
-    error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-    }
-  });
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+        });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
 
   var createTask = function () {
     $.ajax({
       type: 'POST',
-      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=48',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=46',
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
@@ -49,10 +28,72 @@ $(document).ready(function()
         }
       }),
       success: function (response, textStatus) {
-        console.log(response);
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
     });
   }
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
+
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=50',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=50',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=50',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
+    }
+  });
+
+  getAndDisplayAllTasks();
+});
