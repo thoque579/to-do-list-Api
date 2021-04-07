1a8a99b8ef2138b39967e6ec197ac8ea5180d5ee
thoque579 <tusharhoque2@gmail.com>
2021-04-06 18:26:52 -0700

    added a create task function, gets post request form api, and added input

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-    dataType: 'json',
-    success: function (response, textStatus) {
+$(document).ready(function()
+  {
+    $.ajax(
+    {
+      type: 'GET',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+      dataType: 'json',
+      success: function (response, textStatus) {
       response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
+      $('#todo-list').append('<p>' + task.content + '</p>');
       })
     },
     error: function (request, textStatus, errorMessage) {
@@ -14,22 +16,43 @@ $(document).ready(function(){
   });
 
 
-  $.ajax({
-  type: 'POST',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=47',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: JSON.stringify({
-    task: {
+  $.ajax(
+  {
+      type: 'POST',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=47',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+    task:
+    {
       content: 'Do something fun'
     }
   }),
-  success: function (response, textStatus) {
+    success: function (response, textStatus)
+  {
     console.log(response);
   },
-  error: function (request, textStatus, errorMessage) {
+    error: function (request, textStatus, errorMessage) {
     console.log(errorMessage);
-  }
-});
+    }
+  });
 
-});
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=48',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
